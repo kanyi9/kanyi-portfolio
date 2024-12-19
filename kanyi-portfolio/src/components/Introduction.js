f7c9ec05677686400b08f0512a93239cdc3eed35
Peter <peterkanyi19193@gmail.com>
2024-12-19 13:17:33 +0300

    updated introduction

--- kanyi-portfolio/src/components/Introduction.js
+++ kanyi-portfolio/src/components/Introduction.js
@@ -1,10 +1,37 @@
+// import React from 'react';
+// import './Introduction.css';
+
+// const Introduction = () => {
+//     return (
+//         <section className="introduction">
+//             <div className="introduction-content">
+//                 <h1>Peter Kanyi</h1>
+//                 <h2>Full stack developer</h2>
+//                 <p>Passionate and innovative thinker with a track record of building full stack applications and front-end using Python, Flask, 
+// React, Java Script and restful API architecture. Proficient in Python with Flask and JavaScript-based programming, with a 
+// strong background in team building and project management. Skilled in frontend design, with expertise in UI/UX 
+// principles, responsive design, and CSS frameworks like Bootstrap and Tailwind CSS. Experienced in state management 
+// and proficient in build tools like Webpack. Committed to accessibility compliance and passionate about delivering 
+// exceptional user experiences.
+// </p>
+//                 <p>This portfolio showcases my skills, projects, and experiences.</p>
+//             </div>
+//             <div className="introduction-image">
+//                 <img src="./images/kanyi.jpg" alt="Your Name" />
+//             </div>
+//         </section>
+//     );
+// };
+
+// export default Introduction;
+
 import React from 'react';
 import './Introduction.css';
 
 const Introduction = () => {
     return (
         <section className="introduction">
-            <div className="introduction-content">
+            <div className="introduction-content slide-in-left">
                 <h1>Peter Kanyi</h1>
                 <h2>Full stack developer</h2>
                 <p>Passionate and innovative thinker with a track record of building full stack applications and front-end using Python, Flask, 
@@ -13,10 +40,10 @@ strong background in team building and project management. Skilled in frontend d
 principles, responsive design, and CSS frameworks like Bootstrap and Tailwind CSS. Experienced in state management 
 and proficient in build tools like Webpack. Committed to accessibility compliance and passionate about delivering 
 exceptional user experiences.
-</p>
+                </p>
                 <p>This portfolio showcases my skills, projects, and experiences.</p>
             </div>
-            <div className="introduction-image">
+            <div className="introduction-image slide-in-right">
                 <img src="./images/kanyi.jpg" alt="Your Name" />
             </div>
         </section>
@@ -24,3 +51,4 @@ exceptional user experiences.
 };
 
 export default Introduction;
+
