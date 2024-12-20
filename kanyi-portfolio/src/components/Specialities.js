2cb004c1d30bd960de50c9b0585174464855c13d
Peter <peterkanyi19193@gmail.com>
2024-12-20 11:04:30 +0300

    centers the specialities header

--- kanyi-portfolio/src/components/Specialities.js
+++ kanyi-portfolio/src/components/Specialities.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import { faReact, faJsSquare, faPython, faCss3Alt } from '@fortawesome/free-brands-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-// const Specialties = () => {
-//     return (
-//         <section className="specialties">
-//             <h2>Specialties</h2>
-//             <div className="specialties-icons">
-//                 <div className="specialty-icon">
-//                     <FontAwesomeIcon icon={faReact} size="3x" />
-//                     <p>React</p>
-//                 </div>
-//                 <div className="specialty-icon">
-//                     <FontAwesomeIcon icon={faJsSquare} size="3x" />
-//                     <p>JavaScript</p>
-//                 </div>
-//                 <div className="specialty-icon">
-//                     <FontAwesomeIcon icon={faPython} size="3x" />
-//                     <p>Python</p>
-//                 </div>
-//                 <div className="specialty-icon">
-//                     <FontAwesomeIcon icon={faCss3Alt} size="3x" />
-//                     <p>CSS</p>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// };
-
-// export default Specialties;
 
 import React from 'react';
 import { faReact, faJsSquare, faPython, faCss3Alt } from '@fortawesome/free-brands-svg-icons';
