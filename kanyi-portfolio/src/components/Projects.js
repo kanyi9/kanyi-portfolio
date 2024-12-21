d86e3e85bfee0888d2907a8663c0957e00284224
Peter <peterkanyi19193@gmail.com>
2024-12-21 11:55:18 +0300

    adds foodbridge project

--- kanyi-portfolio/src/components/Projects.js
+++ kanyi-portfolio/src/components/Projects.js
@@ -15,8 +15,8 @@ const Projects = () => {
                         <p>Project Description</p>
                         <p>Gari-Zetu Auto Dealership is a full-stack web application designed to provide car enthusiasts with an easy and convenient 
 shopping experience. With a user-friendly interface and seamless navigation, aspiring drivers and car owners can explore 
-a wide range of cars, their specifications, an image of the car they would want to purchase. 
-Technologies used –React in frontend – python and flask API backend</p>
+a wide range of cars, their specifications, an image of the car they would want to purchase.</p> 
+<p>Technologies used –React in frontend – python and flask API backend</p>
                         <a href="https://gari-zetu.vercel.app/" target="_blank" rel="noopener noreferrer">Visit Project</a>
                     </div>
                 </div>
@@ -27,6 +27,11 @@ Technologies used –React in frontend – python and flask API backend</p>
                     <div className="project-details">
                         <h3>Project Title</h3>
                         <p>Project Description</p>
+                        <p>Foodbridge is a fullstack web application that aims to bridge the gap by creating a streamlined
+                            user-friendly platform that connects donors, volunteers and those in need , thereby reducing 
+                            food insecurity and promoting healthier communities
+                        </p>
+                        <p>Technologies used are:- React, flask, SQL Alchemy, JWT, flask mail, Tailwind css</p>
                         <a href="https://foodbridge-project.vercel.app/" target="_blank" rel="noopener noreferrer">Visit Project</a>
                     </div>
                 </div>
