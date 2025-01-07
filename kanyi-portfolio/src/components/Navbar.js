85ba4775cd8240e8e5bb53ccc9ee63f96b2ae052
Peter <peterkanyi19193@gmail.com>
2025-01-07 11:22:09 +0300

    adds changes navbar

--- kanyi-portfolio/src/components/Navbar.js
+++ kanyi-portfolio/src/components/Navbar.js
@@ -2,23 +2,24 @@ import React from 'react';
 import './Navbar.css'; // Import the CSS file for styling
 
 const Navbar = () => {
-    const smoothScrollTo = (targetId) => {
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
+    
     return (
         <nav className="navbar">
             <div className="navbar-logo">
                 <img src="./images/kanyi.jpg" alt="Profile" />
             </div>
             <ul className="navbar-links">
-                <li><button color='black' onClick={() => smoothScrollTo('about')}>About</button></li>
-                <li><a href="#specialties">Specialties</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
+        <li>
+            <a href="https://www.linkedin.com/in/peter-kanyi-9a2003318/" target="_blank" rel="noopener noreferrer">
+                <img src="./images/linkedin.svg" alt="LinkedIn" />
+            </a>
+        </li>
+        <li>
+            <a href="https://github.com/kanyi9" target="_blank" rel="noopener noreferrer">
+                <img src="./images/github.svg" alt="GitHub" />
+            </a>
+        </li>
+    </ul>
         </nav>
     );
 };
