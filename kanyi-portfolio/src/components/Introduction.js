dfc3535631fe4621d0a6b0ae351d577a1320b6e6
Peter <peterkanyi19193@gmail.com>
2025-01-16 10:29:12 +0300

    removes introduction background color

--- kanyi-portfolio/src/components/Introduction.js
+++ kanyi-portfolio/src/components/Introduction.js
@@ -90,6 +90,8 @@
 // };
 
 // export default Introduction;
+
+
 import React from 'react';
 import './Introduction.css';
 
@@ -121,3 +123,32 @@ const Introduction = () => {
 };
 
 export default Introduction;
+
+
+// import React from 'react';
+// import './Introduction.css';
+
+// const Introduction = () => {
+//     return (
+//         <section className="introduction bounce">
+//             <video autoPlay loop muted className="background-video">
+//                 <source src="./images/video1.mp4" type="video/mp4" />
+//                 Your browser does not support the video tag.
+//             </video>
+//             <h1>Peter Kanyi</h1>
+//             <h2>Full Stack Developer</h2>
+//             <p>
+//                 Passionate and innovative thinker with a track record of building full stack applications and front-end using Python, Flask, 
+//                 React, JavaScript, and RESTful API architecture. Proficient in Python with Flask and JavaScript-based programming, with a 
+//                 strong background in team building and project management. Skilled in frontend design, with expertise in UI/UX 
+//                 principles, responsive design, and CSS frameworks like Bootstrap and Tailwind CSS. Experienced in state management 
+//                 and proficient in build tools like Webpack. Committed to accessibility compliance and passionate about delivering 
+//                 exceptional user experiences.
+//             </p>
+//             <p>This portfolio showcases my skills, projects, and experiences.</p>
+//             <img src="./images/kanyi.jpg" alt="Peter Kanyi" />
+//         </section>
+//     );
+// };
+
+// export default Introduction;
